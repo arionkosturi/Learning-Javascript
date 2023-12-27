b0ce5187da8917217f1fd9cbef50995514d01ceb
arkotech <arionkosturi@gmail.com>
2023-12-27 13:12:31 +0100

    Shtuar disa tipe funksioni

--- functions.js
+++ functions.js
@@ -1,6 +1,23 @@
 //Funksionet ne Javascript
+
+
+// Function Expressions
 let funksioni = function(emri){
 return "Pershendetje " + emri;
 }
 
 console.log(funksioni("Petrit"));
+
+
+// Function Declarations
+function functionName(name) {
+  return "Pershendetje " + name;
+  }
+console.log(functionName('Arion'))  
+
+// Arrow Functions
+// Perdoren per te shkurtuar kodin por nuk mund te perdoren "this" ose "arguments" etj.
+const funksioniArrow = (emriPersonit) => { 
+  return `Pershendetje ${emriPersonit}`;
+}
+console.log(funksioniArrow('Ardit'));
