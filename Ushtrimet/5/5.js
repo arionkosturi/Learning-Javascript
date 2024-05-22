62431a48e8bd37d41e12c8fec432e6f938cbdb46
arionkosturi <arionkosturi@gmail.com>
2024-05-22 23:07:57 +0200

    added 5

--- Ushtrimet/5/5.js
+++ Ushtrimet/5/5.js
@@ -1,18 +1,27 @@
-//Cila eshte vlera e x, y, dhe z?
+// Cila eshte vlera e x, y, dhe z?
 function f() {
   return {
   a: 100,
+  // a: 100
   b: [1,2,false],
+  // b: [1,2,false]
   c: undefined
+  // c: undefined
   }
   }
   function g() {
   return {
   x: f(),
+  // { a: 100, b: [ 1, 2, false ], c: undefined }
   y: f().c,
+  // undefined
   z: 'a' + this.y
+  // 'aundefined'
   }
   }
-  const {x, y, z} = g(f());
-  console.log(x,y,z);
-  // ????????
+  const result = {x, y, z} = g(f());
+  /*
+  x: { a: 100, b: [ 1, 2, false ], c: undefined },
+  y: undefined,
+  z: 'aundefined'
+  */
