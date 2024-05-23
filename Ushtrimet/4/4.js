5cf818c3869c5971e0be81ce04a942f5c8d0b5f2
arkotech <arionkosturi@gmail.com>
2024-05-23 12:22:10 +0200

    4 is done

--- Ushtrimet/4/4.js
+++ Ushtrimet/4/4.js
@@ -3,8 +3,16 @@
           //“Nuk lejohet perdorimi i vlerave negative”, 
 //nëse n është më i madh se 10 të gjuhet përjashtimi 
           //“N duhet të jetë më i vogël se 10”.
- 
-function factorial(n) {
 
+function factorial(n) {
+  if(n < 0) throw 'Nuk lejohet perdorimi i vlerave negative!'
+  if(n > 10) throw 'N duhet të jetë më i vogël se 10'
+  if(n==0 || n==1) return 1;
+  return n * factorial(n-1);
 
 }
+  try {
+    console.log(factorial(10));
+  } catch(e) {
+    console.log(e);
+  }
