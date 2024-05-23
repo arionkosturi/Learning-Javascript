cafc8b3ce54d1c0824efc464c5c2b7d87960ed52
arionkosturi <arionkosturi@gmail.com>
2024-05-23 18:53:35 +0200

    ex1

--- Ushtrimet/4/1.js
+++ Ushtrimet/4/1.js
@@ -2,14 +2,10 @@
 const arr = [4, 5, 10, 3, 8, 6];
 let result = [];
 
-function _map(array,dyfisho) {
-  for (let i=0; i< array.length; i++) {
-}
-}
-const dyfisho = function() {
+function _map(arr,f) {
   for (let i = 0; i < arr.length; i++) {
       result.push(arr[i] * 2);
   }
   return result
 }
-console.log(_map(arr,2));
+console.log(_map(result));
